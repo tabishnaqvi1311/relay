9b291172b9059e8af6e62804e8b8fc16ae7a31e8
Tabish Naqvi <tabish.naqvi2003@gmail.com>
2023-04-19 23:35:47 +0530

    added navItems

---
+++ src/constants/constant.js
@@ -0,0 +1,17 @@
+export const navItems = [
+    {
+        id: "logo",
+        content: "RELAY",
+        size: "2xl"
+    },
+    {
+        id: "about",
+        content: "About",
+        size: "md"
+    },
+    {
+        id: "chat",
+        content: "Chat",
+        size: "md"
+    }
+]
